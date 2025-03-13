823f331a07f909e8eab361fe9fe54bd73a072b54
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-03-13 15:34:43 +0900

    Time: 141 ms (13.68%), Space: 27 MB (33.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,9 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        min_price = 10**4 + 1
         profit = 0
-        for p in prices:
-            min_price = min(min_price, p)
-            profit = max(profit, p - min_price)
+        min_price = prices[0]
+        
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = max(profit, price - min_price)
+
         return profit
