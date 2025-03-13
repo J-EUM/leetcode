04681302cf648cf619d3f2ce42a9b3a520b52ca3
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-03-13 17:05:38 +0900

    Time: 2 ms (98.89%), Space: 18 MB (33.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,10 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_dict = {}
-
-        for ch in s:
-            s_dict[ch] = s_dict.get(ch, 0) + 1
-
-        for ch in t:
-            if s_dict.get(ch) is None:
-                return False
-            if s_dict.get(ch) > 1:
-                s_dict[ch] -= 1
-            else:
-                s_dict.pop(ch)
-        
-        if s_dict:
+        if len(s) != len(t):
             return False
-        else:
-            return True
         
+        for ch in set(s):
+            if s.count(ch) != t.count(ch):
+                return False
+        
+        return True
