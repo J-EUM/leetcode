3bd9aff3f35b2082973fe67212652e8e1f1518ff
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-03-13 13:53:37 +0900

    Time: 0 ms (100%), Space: 17.8 MB (80.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,8 +8,11 @@ class Solution:
         }
 
         for ch in s:
-            if stack and stack[-1] == bracket_map.get(ch):
-                stack.pop()
+            if ch in bracket_map:
+                if stack and stack[-1] == bracket_map.get(ch):
+                    stack.pop()
+                else:
+                    return False
             else:
                 stack.append(ch)
         
