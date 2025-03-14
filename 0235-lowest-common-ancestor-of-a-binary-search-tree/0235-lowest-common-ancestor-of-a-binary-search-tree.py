0d415d42cb7ed4901e7d054ef079d4ece9c96e01
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-03-14 15:11:14 +0900

    Time: 64 ms (24.88%), Space: 21 MB (48.18%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,6 +1,3 @@
-from collections import deque
-
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -10,29 +7,15 @@ from collections import deque
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        tree = {}
-        p_index = 0
-        q_index = 0
-        
-        queue = deque([(root, 1)])
-        while p_index == 0 or q_index == 0:
-            node, index = queue.popleft()
-            tree[index] = node
-            if node.val == p.val:
-                p_index = index
-            if node.val == q.val:
-                q_index = index
 
-            if node.left:
-                queue.append((node.left, index * 2))
-            if node.right:
-                queue.append((node.right, index * 2 + 1))
-        
-        while p_index != q_index:
-            print(p_index, q_index)
-            if p_index > q_index:
-                p_index //= 2
+        # p가 왼쪽, q가 오른쪽
+        if p.val > q.val:
+            p, q = q, p
+
+        while root:
+            if p.val <= root.val <= q.val:
+                return root
+            elif p.val >= root.val:
+                root = root.right
             else:
-                q_index //=2
-    
-        return tree[p_index]
+                root = root.left
