9b22fbee185a6180431dad02f945b1951f83aad1
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-03-19 11:38:46 +0900

    Time: 1 ms (98.77%), Space: 17.9 MB (54.58%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,13 +1,19 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        magazine_hash = {}
-
-        for c in magazine:
-            magazine_hash[c] = magazine_hash.get(c, 0) + 1
-        
-        for c in ransomNote:
-            magazine_hash[c] = magazine_hash.get(c, 0) - 1
-            if magazine_hash[c] < 0:
+        for c in set(ransomNote):
+            if ransomNote.count(c) > magazine.count(c):
                 return False
-
         return True
+
+
+        # magazine_hash = {}
+
+        # for c in magazine:
+        #     magazine_hash[c] = magazine_hash.get(c, 0) + 1
+        
+        # for c in ransomNote:
+        #     magazine_hash[c] = magazine_hash.get(c, 0) - 1
+        #     if magazine_hash[c] < 0:
+        #         return False
+
+        # return True
