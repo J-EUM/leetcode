e88e501bc42147a7ed894594b92366fb8389dadd
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-03-19 11:14:10 +0900

    Time: 44 ms (13.68%), Space: 17.6 MB (92.39%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -3,15 +3,12 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        bad_version = n
         start = 1
         end = n
-        while start <= end:
+        while start < end:
             version_to_check = (start + end) // 2
             if isBadVersion(version_to_check):
-                bad_version = min(bad_version, version_to_check)
-                end = version_to_check - 1
+                end = version_to_check
             else:
                 start = version_to_check + 1
-        return bad_version
-      
+        return start
