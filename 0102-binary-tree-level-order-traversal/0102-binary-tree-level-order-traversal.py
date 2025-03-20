9b5631a0f3d61941363f98cd2937ee6d26c10b59
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-03-20 16:30:51 +0900

    Time: 0 ms (100%), Space: 18.5 MB (60.29%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,16 +6,43 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        level_map = {}
+        res = []
 
-        def build_level_map(node, level):
-            if node:
-                if level not in level_map:
-                    level_map[level] = []
-                level_map[level].append(node.val)
-                build_level_map(node.left, level + 1)
-                build_level_map(node.right, level + 1)
+        if not root:
+            return res
+
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            same_level = []
+
+            for _ in range(len(q)):
+                node = q.popleft()
+                same_level.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            res.append(same_level)
+
+        return res
+
+
+
+
+        # level_map = {}
+
+        # def build_level_map(node, level):
+        #     if node:
+        #         if level not in level_map:
+        #             level_map[level] = []
+        #         level_map[level].append(node.val)
+        #         build_level_map(node.left, level + 1)
+        #         build_level_map(node.right, level + 1)
             
-        build_level_map(root, 0)
+        # build_level_map(root, 0)
 
-        return [v for _, v in level_map.items()]
+        # return [v for _, v in level_map.items()]
