003e4d58497a1e2856802c9e5c5ba60569d1a697
J-EUM <97498663+J-EUM@users.noreply.github.com>
2025-04-08 17:53:45 +0900

    Time: 0 ms (100%), Space: 19.3 MB (40.86%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -4,23 +4,26 @@ from collections import defaultdict
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
+        visited = [0] * numCourses  # 0: 미방문, 1: 방문중, 2: 방문완료
         graph = defaultdict(list)
         for a, b in prerequisites:
             graph[a].append(b)
 
-        for u in range(numCourses):
-            for v in graph[u]:
-                indegree[v] += 1
-        
-        q = deque([i for i in range(numCourses) if indegree[i] == 0])
-        count = 0
-        while q:
-            node = q.popleft()
-            count += 1
-            for neighbor in graph[node]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    q.append(neighbor)
+        def dfs(node):  # 사이클이 있는지
+            if visited[node] == 1:
+                return True
+            if visited[node] == 2:
+                return False
 
-        return count == numCourses
+            visited[node] = 1
+            for neighbor in graph[node]:
+                if dfs(neighbor):
+                    return True
+            visited[node] = 2
+            return False
+
+        for i in range(numCourses):
+            if visited[i] == 0:
+                if dfs(i):
+                    return False
+        return True
